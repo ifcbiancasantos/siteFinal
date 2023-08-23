f2b49c84f8139acc2ffb26bf5c40c58014f55e6c
ifcbiancasantos <ifc.bibis@gmail.com>
2023-08-23 09:29:39 -0300

    23/08

--- js/script.js
+++ js/script.js
@@ -10,16 +10,3 @@ function menuShow(){
 }
 
 var button = document.getElementById('read_button');
-
-// Click
-button.addEventListener('click', function() {
-    var card = document.querySelector('.card');
-
-    card.classList.toggle('active');
-
-    if (card.classList.contains('active')) {
-        return button.textContent = 'Read less';
-    }
-    
-    button.textContent = 'Read more';
-});
